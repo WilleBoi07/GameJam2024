9630747d255bc8e7d2923bc6315c87289b5e0558
WilleBoi07 <jonssonw07@outlook.com>
2024-11-05 13:54:06 +0100

    Black Jack Scen

--- Assets/BJTrigger.cs
+++ Assets/BJTrigger.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class BJTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    bool in_blackjack = false;
+    private void OnTriggerEnter2D(Collider Other)
     {
-        
+        in_blackjack = true;
     }
+
+    private void OnTriggerExit2D(Collider other)
+    {
+        in_blackjack = false;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (in_blackjack = true)
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
     }
 }
+
